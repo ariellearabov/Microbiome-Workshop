ce294a6df8232210e446a3358ae874a46333d9fc
arabov <arabov@mail.tau.ac.il>
2023-04-27 13:57:18 +0300

    wrote missing data exploration func

--- data_exploration.py
+++ data_exploration.py
@@ -8,13 +8,12 @@ def main():
     meta = "https://drive.google.com/drive/folders/1YU84ZOk_BNGIOxNrATO_VhtLkHSlv-w3?usp=share_link"
     #get_data(omic, meta)  # doesn't work since python has no premission to acsess the folders so we downloaded them manually. 
     
-    os.chdir("data")
+    #os.chdir("data")
     paths = os.listdir()
     dfs = upload_dfs(paths)
-
-
-
-    
+    peek_at_data(dfs)
+    missing_data_exploration(dfs)
+  
 
 def get_data(url1, url2):
     gdown.download_folder(url1, quiet=True, use_cookies=False)
@@ -26,7 +25,7 @@ def upload_dfs(paths):
     for i in range(len(relevant_paths)):
         path = relevant_paths[i]
         if path == "kegg_names.txt":
-            dfs.append((pd.read_fwf(path, header=None, names=["ID", "pathway_name"]), path))
+            dfs.append((pd.read_fwf(path, header=None, names=["SampleID", "pathway_name"]), path))
         elif path == "metadata.txt":
             dfs.append((pd.read_table(path, header=0), path))
         else:
@@ -39,8 +38,17 @@ def peek_at_data(dfs_list):
         print(f'no. of samples: %d, no. of {elm[1].split(".")[0]+"_cols"}: %d ' % elm[0].shape)
 
 
-def missing_data_exploration():
-    return 0
+def missing_data_exploration(dfs_list):
+    for elm in dfs_list:
+        df = elm[0]
+        if (("metadata" in elm[1]) or ("kegg_names" in elm[1])):
+            continue
+        df[df == 0] = None 
+        total_missing = np.sum(np.isnan(df.values))
+        total_obs = np.prod(df.shape)
+        print(f'In {elm[1].split(".")[0]}-')
+        print('toal missing values: %d out of toal observations: %d' % (total_missing, total_obs))
+        print('so that\'s %.2f missing \n' % (total_missing/total_obs))
 
 if __name__ == "__main__":
     main()
